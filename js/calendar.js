c912d24929e51ce9e583e1b6de39cee27181c3b4
Vihodets <max.dpkg@gmail.com>
2021-02-10 22:14:07 +0200

    first test task javascript

---
+++ js/calendar.js
@@ -0,0 +1,99 @@
+document.querySelector('#new-event').onclick = (event) => {
+    event.preventDefault();
+    window.open('./create-event.html', "_self");
+}
+
+let eventName = localStorage.getItem('eventName');
+let participants = localStorage.getItem('participants');
+let day = localStorage.getItem('day');
+let time = localStorage.getItem('time');
+
+
+function addEvent() {
+    let array = JSON.parse(localStorage.getItem("array"));
+    if (array) {
+        const span = "<span class=\"close\">&times;</span>";
+        let table = document.querySelector('#tableCalendar');
+        let tr = table.querySelectorAll('tr');
+
+        for (let i = 1; i < tr.length; i++){
+            let td = tr[i].querySelectorAll('td');
+            for (let j = 1; j < td.length; j++) {
+                if (array[i][j] != "") {
+                    td[j].innerHTML = array[i][j].eventName + span;
+                    td[j].classList.add("active-cell");
+                }
+            }
+        }
+    }
+}
+
+addEvent();
+
+function delEvent() {
+    let array = JSON.parse(localStorage.getItem("array"));
+    let table = document.querySelector('#tableCalendar');
+    let tr = table.querySelectorAll('tr');
+
+    for (let i = 1; i < tr.length; i++){
+        let td = tr[i].querySelectorAll('td');
+        for (let j = 1; j < td.length; j++) {
+            if (td[j].querySelector('.close')) {
+                td[j].querySelector('.close').onclick = function () {
+
+                    document.querySelector(".modal-wrapper").style.display = "block";
+                    let text = "Are you sure you want to delete \""+ array[i][j].eventName + "\" event?"
+                    document.querySelector(".modal-text").textContent = text;
+
+                    document.querySelector(".button-no").onclick = function () {
+                        document.querySelector(".modal-wrapper").style.display = "none";
+                    }
+
+                    document.querySelector(".button-yes").onclick = function () {
+                        document.querySelector(".modal-wrapper").style.display = "none";
+
+                        td[j].innerHTML = "";
+                        td[j].classList.remove("active-cell");
+                        array[i][j] = "";
+                        localStorage.setItem('array', JSON.stringify(array));
+                    }
+
+                }
+            }
+        }
+    }
+
+
+}
+
+delEvent();
+
+// Function that filter participants
+document.querySelector('#user-select').onchange = function() {
+    console.log("Click select");
+    let participantsIndex = document.querySelector('#user-select').selectedIndex;
+    let participants = document.querySelector('#user-select').options[participantsIndex].text;
+    let array = JSON.parse(localStorage.getItem("array"));
+    if (array) {
+        const span = "<span class=\"close\">&times;</span>";
+        let table = document.querySelector('#tableCalendar');
+        let tr = table.querySelectorAll('tr');
+
+        for (let i = 1; i < tr.length; i++){
+            let td = tr[i].querySelectorAll('td');
+            for (let j = 1; j < td.length; j++) {
+                if (array[i][j] != "" && participants === "All members"){
+                    td[j].innerHTML = array[i][j].eventName + span;
+                    td[j].classList.add("active-cell");
+                } else if (array[i][j] != "" && array[i][j].participants === participants) {
+                    td[j].innerHTML = array[i][j].eventName + span;
+                    td[j].classList.add("active-cell");
+                } else {
+                    td[j].innerHTML = "";
+                    td[j].classList.remove("active-cell");
+                }
+
+            }
+        }
+    }
+}
---
+++ js/create-event.js
@@ -0,0 +1,68 @@
+document.querySelector('#create').onclick = (event) => {
+    event.preventDefault();
+
+    let eventName = document.getElementById('eventName').value;
+    let participantsIndex = document.getElementById('participants').selectedIndex;
+    let participants = document.getElementById('participants').options[participantsIndex].text;
+
+    let dayIndex = document.getElementById('day').selectedIndex;
+    let day = document.getElementById('day').options[dayIndex].text;
+
+    let timeIndex = document.getElementById('time').selectedIndex;
+    let time = document.getElementById('time').options[timeIndex].text;
+
+    let array = JSON.parse(localStorage.getItem("array"));
+    //console.log(array);
+    if (eventName) {
+
+        for (let i = 0; i < array.length; i++){
+            for (let j = 0; j < array[0].length; j++) {
+                //console.log(array[i][j]);
+                if (array[0][j] === day && array[i][0] === time) {
+                    if (array[i][j] === ""){
+                        array[i][j] = {'eventName': eventName, 'participants': participants};
+                        localStorage.setItem('array', JSON.stringify(array));
+                        localStorage.setItem('i', i);
+                        localStorage.setItem('j', j);
+                        console.log(i, j, array[i][j]);
+
+                        localStorage.setItem('eventName', eventName);
+                        localStorage.setItem('participants', participants);
+                        localStorage.setItem('day', day);
+                        localStorage.setItem('time', time);
+
+                        console.log(eventName);
+                        console.log(participants);
+                        console.log(day);
+                        console.log(time);
+
+                        window.open('./calendar.html', "_self");
+                    } else {
+                        popupError();
+                    }
+                }
+            }
+        }
+    } else {
+        popupError("Failed to create an event. Name of the event not specified.");
+    }
+}
+
+function popupError(text) {
+    let popup = document.getElementById("popup-error");
+    let close = document.querySelector('.cross2');
+    if (text){
+        document.querySelector('#text').textContent = text;
+    }
+
+    popup.style.display = "flex";
+    close.onclick = function (){
+        popup.style.display = "none";
+    }
+}
+
+document.querySelector('#cancel').onclick = (event) => {
+    event.preventDefault();
+
+    window.open('./calendar.html', "_self");
+}
